7fa2744069f5ff766be07b902fcf00ab4ceae1f2
jona8690 <jona8690@edu.eal.dk>
2016-11-18 13:30:23 +0100

    Console with buffer system

--- Code/Interface/Program.cs
+++ Code/Interface/Program.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Interface {
 	class Program {
+		Dictionary<int, string> CB = new Dictionary<int, string>();
 		static void Main(string[] args) {
+			Program A = new Program();
+			A.Run();
+		}
+	
+		private void WriteLine(string Line) {
+			int HK = CB.Keys.Last();
+			CB.Add(HK++, Line);
+		}
+
+		private void WriteConsole(bool clear = true) {
+			if(clear) Console.Clear();
+			foreach(var Line in CB) {
+				Console.WriteLine(Line.Value);
+			}
+		}
+
+		private void Clear() {
+			CB.Clear();
+		}
+
+		private void Run() {
+			WriteLine("TEST");
+			WriteLine("TEST2");
+			WriteLine("Test3");
+			WriteConsole(false);
 		}
 	}
 }
