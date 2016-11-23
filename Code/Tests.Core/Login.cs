d7ad6f1706dc5455a4ecf85298ecf2452c8f905a
jona8690 <jona8690@edu.eal.dk>
2016-11-23 12:37:21 +0100

    Added test for exception on login

--- Code/Tests.Core/Login.cs
+++ Code/Tests.Core/Login.cs
@@ -37,6 +37,26 @@ namespace Tests.Core {
 			Assert.AreEqual(A, AL);
 		}
 
+		[TestMethod]
+		public void TestLoginNoUser() {
+			Address Address_A = new Address(38, "Grønløkkevej", 6800, "Odense");
+			Employee A = new Employee(1, "test@example.com", "1234", "Test", "User", Address_A, "12344567", Permissions.Employee);
+			EmployeeRepository.SaveEmployee(A);
+
+			Address Address_B = new Address(1, "Grønløkkevej", 6800, "Odense");
+			Employee B = new Employee(2, "test2@example.com", "1235", "Test2", "User2", Address_B, "23456789", Permissions.Manager);
+			EmployeeRepository.SaveEmployee(B);
+
+			try {
+				Employee AL = EmployeeRepository.Login("tes3t@example.com", "1234");
+				Assert.IsTrue(false);
+			} catch(Exception e) {
+				if(e.Message == "User Not Found") {
+					Assert.IsTrue(true);
+				}
+			}
+		}
+
 
 		[TestMethod]
 		public void TestCanUpdateCustomer() {
