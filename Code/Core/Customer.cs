d7e2fc8c0c20f5fc36b40d0075df45c1277d2018
tiramiso <info.appcocos@gmail.com>
2016-11-24 10:41:15 +0100

    Customer Class

---
+++ Code/Core/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public Address Address { get; set; }
+        public string Phone { get; set; }
+
+        public Customer(int id, string email, string password, string firstname, string lastname, Address address, string phone)
+        {
+            this.ID = id;
+            this.Email = email;
+            this.Password = password;
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+            this.Address = address;
+            this.Phone = phone;
+        }
+    }
+}
