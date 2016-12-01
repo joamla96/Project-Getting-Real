ab41b71c5285800528fc5352e0b69cd9aafcb69b
pedr0206 <pedr0209@edu.eal.dk>
2016-12-01 12:50:21 +0100

    database finish Runsp method

---
+++ Code/Core/Database.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Core
+{
+    public class Database
+    {
+        private const string ConnInfo = "Server=ealdb1.eal.local; Database=ejl86_usr; Passowrd=Baz1nga86";
+        private SqlConnection Conn;
+
+        //Database()
+        //{
+        //    Conn = new SqlConnection(ConnInfo);
+        //}
+
+        public void RunSP(string name, Dictionary<string,string> param)
+        {
+            using (Conn = new SqlConnection(ConnInfo))
+            {
+                SqlCommand cmd = new SqlCommand(name, this.Conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                foreach(KeyValuePair<string,string> entry in param)
+                {
+                    cmd.Parameters.Add(new SqlParameter(entry.Key, entry.Value));
+                }
+                try
+                {
+                    Conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                finally
+                {
+                    Conn.Close();
+                }         
+            }                
+        }
+    }
+}
