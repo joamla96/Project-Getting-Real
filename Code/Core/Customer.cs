217e9a83850439f36cad6ee1df10081fdf33229c
Jonas Laursen <jona8690@edu.eal.dk>
2016-12-07 18:48:17 +0100

    Removed some unnessesarry usings

--- Code/Core/Customer.cs
+++ Code/Core/Customer.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-
-namespace Core
-{
-    public class Customer
+﻿namespace Core {
+	public class Customer
     {
         public int ID { get; set; }
         public string Email { get; set; }
